74f40981dd8cc461b31cb624f88b333ac30ad390
Jeffrey Crane <jeffrey.crane@wpengine.com>
2020-12-30 11:59:16 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup
 import requirements
-import os
-
-# read the contents of your README file
 from os import path
+
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -16,8 +15,19 @@ with open('build/requirements.txt') as f:
             full_line = name + ''.join([''.join(list(spec)) for spec in req.specs])
             install_requires.append(full_line)
 
-setup(name="Let's Debug Helper",
-      version=os.getenv('VERSION'),
+setup(name="lets-debug-helper",
+      version="1.0.2",
+      author="Jeffrey Crane",
+      author_email="jediknight11206@gmail.com",
+      description="This is a cli tool that interacts with the Let's Debug API",
+      url="https://github.com/jediknight112/lets-debug-helper",
       install_requires=install_requires,
       long_description=long_description,
-      long_description_content_type='text/markdown')
+      long_description_content_type='text/markdown',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.8',
+      )
