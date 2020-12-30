1087f4fd42a3f4449b4e9f6ee94caecf444bb27c
Jeffrey Crane <jeffrey.crane@wpengine.com>
2020-12-30 11:41:01 -0600

    updating readme, adding licence, attempt at pypi discription

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 import requirements
 import os
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 install_requires = []
 with open('build/requirements.txt') as f:
     for req in requirements.parse(f):
@@ -10,5 +16,8 @@ with open('build/requirements.txt') as f:
             full_line = name + ''.join([''.join(list(spec)) for spec in req.specs])
             install_requires.append(full_line)
 
-setup(version=os.getenv('VERSION'),
-      install_requires=install_requires)
+setup(name="Let's Debug Helper",
+      version=os.getenv('VERSION'),
+      install_requires=install_requires,
+      long_description=long_description,
+      long_description_content_type='text/markdown')
